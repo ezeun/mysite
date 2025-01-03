ae8c78b2f818c68d45ddfdcd0e56bf067356c060
ezeun <consistency1231@gmail.com>
2025-01-03 15:32:32 +0900

    site #2

--- mysite03/src/main/java/mysite/service/SiteService.java
+++ mysite03/src/main/java/mysite/service/SiteService.java
@@ -2,13 +2,19 @@ package mysite.service;
 
 import org.springframework.stereotype.Service;
 
+import mysite.repository.SiteRepository;
 import mysite.vo.SiteVo;
 
 @Service
 public class SiteService {
+	private SiteRepository siteRepository;
+	
+	public SiteService(SiteRepository siteRepository) {
+		this.siteRepository = siteRepository;
+	}
 	
 	public SiteVo getSite() {
-		return null;
+		return siteRepository.findOne();
 	}
 	
 	public void updateSite(SiteVo siteVo) {
