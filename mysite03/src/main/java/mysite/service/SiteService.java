b014a51819ce1809a7d34a07b1d2d5a957d76b0f
ezeun <consistency1231@gmail.com>
2025-01-03 15:11:14 +0900

    site #1

---
+++ mysite03/src/main/java/mysite/service/SiteService.java
@@ -0,0 +1,17 @@
+package mysite.service;
+
+import org.springframework.stereotype.Service;
+
+import mysite.vo.SiteVo;
+
+@Service
+public class SiteService {
+	
+	public SiteVo getSite() {
+		return null;
+	}
+	
+	public void updateSite(SiteVo siteVo) {
+		
+	}
+}
