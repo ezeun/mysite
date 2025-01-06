0e61a852f9b8faaa4e7294d6f68ba053590b23eb
ezeun <consistency1231@gmail.com>
2025-01-06 16:36:43 +0900

    SiteInterceptor

--- mysite03/src/main/java/mysite/event/ApplicationContextEventListener.java
+++ mysite03/src/main/java/mysite/event/ApplicationContextEventListener.java
@@ -6,6 +6,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 
 import mysite.service.SiteService;
+import mysite.vo.SiteVo;
 
 public class ApplicationContextEventListener {
 	@Autowired
@@ -14,6 +15,13 @@ public class ApplicationContextEventListener {
 	@EventListener({ContextRefreshedEvent.class})
 	public void handlerContextRefreshedEvent() {
 		SiteService siteService = applicationContext.getBean(SiteService.class);
+		SiteVo siteVo = siteService.getSite();
+		
+        // ApplicationContext에 SiteVo를 Bean으로 등록
+        ((org.springframework.beans.factory.support.DefaultListableBeanFactory) 
+            applicationContext.getAutowireCapableBeanFactory())
+            .registerSingleton("siteVo", siteVo);
+		
 		System.out.println("-- ContextRefreshedEvent Received --" + siteService);
 	}
 }
